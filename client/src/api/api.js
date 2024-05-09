7156facf1b53452d313874b3cf60637163076ba1
Jyotikeshri <156110025+Jyotikeshri@users.noreply.github.com>
2024-05-10 00:59:34 +0530

    idk4

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: `https://metube-i2wi.onrender.com/` });
+const API = axios.create({ baseURL: `https://me-tube-gffn.onrender.com/` });
 // const API = axios.create({ baseURL: " http://localhost:8080" });
 // const API = axios.create({
 //   baseURL: `https://youtubeclone5031.herokuapp.com/`,
